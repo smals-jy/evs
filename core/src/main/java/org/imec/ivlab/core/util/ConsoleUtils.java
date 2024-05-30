7d6f31f800ee111abe794be4d201b5399469f9c5
Jacques Yakoub <jacques.yakoub@gmail.com>
2024-05-30 20:01:19 +0200

    perf: remove com.inamik.text.tables (#358)
    * perf: simplify ConsoleUtils.java
    
    * Delete local-maven-repo directory
    
    * perf: remove local maven repo
    
    * chore: remove com.github.inamik.text.tables
    
    * chore: KmehrValidationFormatter.java
    
    * chore: Update FileWithValidatedKmehrsFormatter.java
    
    * chore: Update maven.yml
    
    * Update ConsoleUtils.java

--- core/src/main/java/org/imec/ivlab/core/util/ConsoleUtils.java
+++ core/src/main/java/org/imec/ivlab/core/util/ConsoleUtils.java
@@ -1,9 +1,5 @@
 package org.imec.ivlab.core.util;
 
-import com.inamik.text.tables.GridTable;
-import com.inamik.text.tables.SimpleTable;
-import com.inamik.text.tables.grid.Border;
-import com.inamik.text.tables.grid.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.imec.ivlab.core.authentication.AuthenticationConfigReader;
@@ -15,40 +11,42 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-
-import static com.inamik.text.tables.Cell.Functions.HORIZONTAL_CENTER;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsoleUtils {
 
     private final static Logger log = LogManager.getLogger(AuthenticationConfigReader.class);
 
-
     private static final int BOX_LENGTH = 170;
 
     public static String emphasizeTitle(String message) {
-
-        SimpleTable simpletable = SimpleTable.of();
-        simpletable.nextRow()
-                .nextCell()
-                .addLine("");
-
+        StringBuilder builder = new StringBuilder();
+        builder.append(createBorder(BOX_LENGTH)).append(System.lineSeparator());
         String[] lines = message.split(System.lineSeparator());
-        for (int i = 0; i < lines.length; i++) {
-            simpletable.addLine(lines[i]).applyToCell(HORIZONTAL_CENTER.withWidth(BOX_LENGTH).withChar(' '));
+        for (String line : lines) {
+            builder.append(center(line, BOX_LENGTH)).append(System.lineSeparator());
         }
-        simpletable.addLine("");
-
-        GridTable gridTable = simpletable.toGrid();
-        gridTable = Border.of(Border.Chars.of('+', '-', '|')).apply(gridTable);
-        return tableToString(gridTable);
-
+        builder.append(createBorder(BOX_LENGTH));
+        return builder.toString();
     }
 
-    private static String tableToString(GridTable table) {
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        PrintStream outStream = new PrintStream(byteOut);
-        Util.print(table, outStream);
-        return byteOut.toString();
+    private static String repeat(String str, int length) {
+        IntFunction<String> repeater = (l) -> IntStream.range(0, l)
+                .mapToObj(i -> str)
+                .collect(Collectors.joining());
+        return repeater.apply(length);
+    }
+    
+    private static String center(String text, int length) {
+        int paddingSize = (length - text.length()) / 2;
+        String padding = repeat(" ", Math.max(0, paddingSize));
+        return "|" + padding + text + padding + (text.length() % 2 == 0 ? padding : padding + " ") + "|";
+    }
+
+    private static String createBorder(int length) {
+        return "+" + repeat("-",length) + "+";
     }
 
     public static String createAsciiMessage(String message) {
@@ -56,26 +54,25 @@ public class ConsoleUtils {
         Graphics g = image.getGraphics();
         g.setFont(new Font("Dialog", Font.PLAIN, 24));
         Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         graphics.drawString(message, 6, 24);
-        try {
-            ImageIO.write(image, "png", new File("text.png"));
-        } catch (IOException e) {
-            log.error(e);
-            return "";
-        }
 
         StringBuffer stringBuffer = new StringBuffer();
-
         for (int y = 0; y < 32; y++) {
             StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < 144; x++)
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : image.getRGB(x, y) == -1 ? "#" : "*");
+            for (int x = 0; x < 144; x++) {
+                int pixel = image.getRGB(x, y);
+                if (pixel == Color.BLACK.getRGB()) {
+                    sb.append(" ");
+                } else if (pixel == Color.WHITE.getRGB()) {
+                    sb.append("#");
+                } else {
+                    sb.append("*");
+                }
+            }
             if (sb.toString().trim().isEmpty()) continue;
-            stringBuffer.append(sb.toString() + System.lineSeparator());
+            stringBuffer.append(sb.toString()).append(System.lineSeparator());
         }
-
         return stringBuffer.toString();
     }
 
--- validator/src/main/java/org/imec/ivlab/validator/report/KmehrValidationFormatter.java
+++ validator/src/main/java/org/imec/ivlab/validator/report/KmehrValidationFormatter.java
@@ -1,9 +1,5 @@
 package org.imec.ivlab.validator.report;
 
-import com.inamik.text.tables.GridTable;
-import com.inamik.text.tables.SimpleTable;
-import com.inamik.text.tables.grid.Border;
-import com.inamik.text.tables.grid.Util;
 import org.imec.ivlab.validator.validators.model.AbstractValidationItem;
 import org.imec.ivlab.validator.validators.model.ValidationResult;
 import org.imec.ivlab.validator.validators.business.rules.model.ExecutionStatus;
@@ -12,14 +8,13 @@ import org.imec.ivlab.validator.validators.xsd.model.XsdFailure;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.Objects;
-
-import static com.inamik.text.tables.Cell.Functions.HORIZONTAL_CENTER;
 
 public class KmehrValidationFormatter {
 
     public static String getOutput(ValidationResult validationResult) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append(createRuleSummaryTable(validationResult));
         sb.append(System.lineSeparator());
         sb.append(createValidationDetailsTable(validationResult));
@@ -27,102 +22,53 @@ public class KmehrValidationFormatter {
     }
 
     private static String createRuleSummaryTable(ValidationResult validationResult) {
-
-        SimpleTable s = SimpleTable.of();
-
-        s.nextRow()
-                .nextCell()
-                    .addLine("Pass").applyToCell(HORIZONTAL_CENTER.withWidth(10).withChar(' '))
-                .nextCell()
-                    .addLine("Fail")
-                .nextCell()
-                    .addLine("Interrupted");
-        s.nextRow()
-                .nextCell()
-                    .addLine(String.valueOf(validationResult.getPassedList().size()))
-                .nextCell()
-                    .addLine(String.valueOf(validationResult.getFailedList().size()))
-                .nextCell()
-                    .addLine(String.valueOf(validationResult.getInterruptedList().size()));
-
-
-        GridTable gridTable = s.toGrid();
-        gridTable = Border.of(Border.Chars.of('+', '-', '|')).apply(gridTable);
-        return tableToString(gridTable);
-
-    }
-
-    private static String tableToString(GridTable table) {
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        PrintStream outStream = new PrintStream(byteOut);
-        Util.print(table, outStream);
-        return byteOut.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("+----------+------+-------------+\n");
+        sb.append("|   Pass   | Fail | Interrupted |\n");
+        sb.append("+----------+------+-------------+\n");
+        sb.append(String.format("| %8d | %4d | %11d |\n",
+                validationResult.getPassedList().size(),
+                validationResult.getFailedList().size(),
+                validationResult.getInterruptedList().size()));
+        sb.append("+----------+------+-------------+");
+        return sb.toString();
     }
 
     private static String createValidationDetailsTable(ValidationResult validationResult) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("+----------+-------+------------------------------------------+----------------+\n");
+        sb.append("|  RuleId  | Level |           Message                        | ExecutionStatus |\n");
+        sb.append("+----------+-------+------------------------------------------+----------------+\n");
 
-        SimpleTable s = SimpleTable.of();
+        validationResultToRows(validationResult, sb);
 
-        s.nextRow()
-                .nextCell()
-                .addLine("RuleId").applyToCell(HORIZONTAL_CENTER.withWidth(10).withChar(' '))
-                .nextCell()
-                .addLine("Level")
-                .nextCell()
-                .addLine("Message")
-                .nextCell()
-                .addLine("ExecutionStatus");
-
-
-        validationResultToRows(validationResult, s);
-
-        GridTable gridTable = s.toGrid();
-        gridTable = Border.of(Border.Chars.of('+', '-', '|')).apply(gridTable);
-        return tableToString(gridTable);
-
+        sb.append("+----------+-------+------------------------------------------+----------------+");
+        return sb.toString();
     }
 
-    private static void validationResultToRows(ValidationResult validationResult, SimpleTable s) {
+    private static void validationResultToRows(ValidationResult validationResult, StringBuilder sb) {
+        appendValidationItems(validationResult.getFailedList(), sb);
+        appendValidationItems(validationResult.getInterruptedList(), sb);
+    }
 
-        for (AbstractValidationItem validationItem : validationResult.getFailedList()) {
-
-            s.nextRow();
-
-            if (validationItem.getClass().isAssignableFrom(RuleResult.class)) {
+    private static void appendValidationItems(List<? extends AbstractValidationItem> items, StringBuilder sb) {
+        for (AbstractValidationItem validationItem : items) {
+            if (validationItem instanceof RuleResult) {
                 RuleResult ruleResult = (RuleResult) validationItem;
-
-                s.nextCell().addLine(ruleResult.getRuleId())
-                        .nextCell().addLine(ruleResult.getLevel().name())
-                        .nextCell().addLine(ruleResult.getMessage())
-                        .nextCell().addLine(ruleResult.getExecutionStatus().name());
-
-            }
-
-            if (validationItem.getClass().isAssignableFrom(XsdFailure.class)) {
+                sb.append(String.format("| %s | %s | %s | %s |\n",
+                        ruleResult.getRuleId(),
+                        ruleResult.getLevel().name(),
+                        ruleResult.getMessage(),
+                        ruleResult.getExecutionStatus().name()));
+            } else if (validationItem instanceof XsdFailure) {
                 XsdFailure xsdFailure = (XsdFailure) validationItem;
-
-                s.nextCell().addLine("")
-                        .nextCell().addLine(xsdFailure.getLevel().name())
-                        .nextCell().addLine(xsdFailure.getMessage())
-                        .nextCell().addLine(ExecutionStatus.FAIL.name());
-
+                sb.append(String.format("| %s | %s | %s | %s |\n",
+                        "",
+                        xsdFailure.getLevel().name(),
+                        xsdFailure.getMessage(),
+                        ExecutionStatus.FAIL.name()));
             }
-
-
         }
-
-        for (RuleResult ruleResult : validationResult.getInterruptedList()) {
-
-            s.nextRow();
-
-            s.nextCell().addLine(Objects.toString(ruleResult.getRuleId(), ""))
-                    .nextCell().addLine(ruleResult.getLevel().name())
-                    .nextCell().addLine(ruleResult.getMessage())
-                    .nextCell().addLine(ruleResult.getExecutionStatus().name());
-
-
-        }
-
     }
 
 }
