3812f48ac0b7c946134b72239b84d6531c0da7fb
smals-jy <143497095+smals-jy@users.noreply.github.com>
2024-08-27 09:41:20 +0200

    Update Treatment.java (#461)
    Lombok

--- core/src/main/java/org/imec/ivlab/core/model/internal/parser/sumehr/Treatment.java
+++ core/src/main/java/org/imec/ivlab/core/model/internal/parser/sumehr/Treatment.java
@@ -3,26 +3,13 @@ package org.imec.ivlab.core.model.internal.parser.sumehr;
 import org.imec.ivlab.core.model.internal.parser.ItemParsedItem;
 
 import org.joda.time.LocalDate;
+import lombok.Data;
 
+@Data
 public class Treatment extends ItemParsedItem {
 
     private LocalDate beginmoment;
     private LocalDate endmoment;
-
-    public LocalDate getBeginmoment() {
-        return beginmoment;
-    }
-
-    public void setBeginmoment(LocalDate beginmoment) {
-        this.beginmoment = beginmoment;
-    }
-
-    public LocalDate getEndmoment() {
-        return endmoment;
-    }
+    private boolean noKnownTreatment;
     
-    public void setEndmoment(LocalDate endmoment) {
-        this.endmoment = endmoment;
-    }
-
 }
