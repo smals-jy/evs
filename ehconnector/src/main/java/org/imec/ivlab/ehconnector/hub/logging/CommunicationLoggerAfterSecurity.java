faacf85c3c8a6b4ea80e9118589b0edd753e70a5
Jacques Yakoub <jacques.yakoub@gmail.com>
2024-09-04 13:49:56 +0200

    Update CommunicationLoggerAfterSecurity.java

--- ehconnector/src/main/java/org/imec/ivlab/ehconnector/hub/logging/CommunicationLoggerAfterSecurity.java
+++ ehconnector/src/main/java/org/imec/ivlab/ehconnector/hub/logging/CommunicationLoggerAfterSecurity.java
@@ -1,9 +1,16 @@
 package org.imec.ivlab.ehconnector.hub.logging;
 
+import static org.imec.ivlab.ehconnector.hub.logging.Kind.WITH_SECURITY;
+
 import be.ehealth.technicalconnector.exception.TechnicalConnectorException;
 import be.ehealth.technicalconnector.handler.AbstractSOAPHandler;
 import be.ehealth.technicalconnector.utils.ConnectorXmlUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.imec.ivlab.core.config.EVSConfig;
+import org.imec.ivlab.core.config.EVSProperties;
+import org.imec.ivlab.core.model.hub.LogCommunicationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,43 +24,57 @@ public class CommunicationLoggerAfterSecurity extends AbstractSOAPHandler {
 
 
     public boolean handleOutbound(SOAPMessageContext context) {
-
+        if (getLogCommunicationType().equals(LogCommunicationType.WITH_SECURITY) || getLogCommunicationType().equals(LogCommunicationType.ALL)) {
+            logMessage(getMessageText(context.getMessage()), getAction(context.getMessage()), WITH_SECURITY);
+        }
         return true;
     }
 
     public boolean handleInbound(SOAPMessageContext context) {
-
-        SOAPMessage msg = context.getMessage();
-        String messageText;
-        try {
-            messageText = ConnectorXmlUtils.format(ConnectorXmlUtils.toString(msg.getSOAPPart().getEnvelope()));
-        } catch (TechnicalConnectorException | SOAPException e) {
-            LOG.error("Failed to log incoming message", e);
-            messageText = ExceptionUtils.getStackTrace(e);
+        if (getLogCommunicationType().equals(LogCommunicationType.WITH_SECURITY) || getLogCommunicationType().equals(LogCommunicationType.ALL)) {
+            logMessage(getMessageText(context.getMessage()), getAction(context.getMessage()), WITH_SECURITY);
         }
+        return true;
+    }
 
+    private static String getAction(SOAPMessage msg) {
         String action = null;
         try {
-            action = msg.getSOAPPart().getEnvelope().getBody().getFirstChild().getLocalName();
+            action = msg
+                .getSOAPPart().getEnvelope().getBody().getFirstChild().getLocalName();
         } catch (SOAPException e) {
             action = "unknown";
         }
-
-        logMessage(messageText, action);
-
-        return true;
-
+        return action;
     }
 
-    private void logMessage(String messageText, String action) {
+    private static String getMessageText(SOAPMessage msg) {
+        String messageText;
+        try {
+            messageText = ConnectorXmlUtils.format(ConnectorXmlUtils.toString(msg
+                .getSOAPPart().getEnvelope()));
+        } catch (TechnicalConnectorException | SOAPException e) {
+            LOG.error("Failed to log outgoing message", e);
+            messageText = ExceptionUtils.getStackTrace(e);
+        }
+        return messageText;
+    }
+
+    private void logMessage(String messageText, String action, Kind kind) {
         CommunicationLoggerConfiguration instance = CommunicationLoggerConfiguration.getInstance();
         if (!instance.isIgnored(action)) {
-            MessageWriter.logMessage(messageText, action);
+            MessageWriter.logMessage(messageText, action, kind);
         }
     }
 
+    @Override
     public boolean handleFault(SOAPMessageContext c) {
         this.handleMessage(c);
         return true;
+    }
+
+    private LogCommunicationType getLogCommunicationType() {
+        return  LogCommunicationType.fromValue(StringUtils.lowerCase(EVSConfig
+            .getInstance().getProperty(EVSProperties.LOG_COMMUNICATION_TYPE)));
     }
 }
