cb43198015ddb2afd585249fe62a83619f22450e
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-03-23 08:28:45 +0100

    Update ConfigDetails.java

--- core/src/main/java/org/imec/ivlab/core/authentication/model/ConfigDetails.java
+++ core/src/main/java/org/imec/ivlab/core/authentication/model/ConfigDetails.java
@@ -1,8 +1,8 @@
 package org.imec.ivlab.core.authentication.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="authenticationConfiguration")
 @XmlAccessorType(XmlAccessType.FIELD)
