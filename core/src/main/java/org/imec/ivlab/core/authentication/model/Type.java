4388d90b9eea48c45e594371e4feb09b3ed3543b
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-03-23 08:34:14 +0100

    Update Type.java

--- core/src/main/java/org/imec/ivlab/core/authentication/model/Type.java
+++ core/src/main/java/org/imec/ivlab/core/authentication/model/Type.java
@@ -1,8 +1,8 @@
 package org.imec.ivlab.core.authentication.model;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlEnum;
+import jakarta.xml.bind.annotation.XmlEnumValue;
+import jakarta.xml.bind.annotation.XmlType;
 
 @XmlType
 @XmlEnum(String.class)
