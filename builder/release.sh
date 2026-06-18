ee95ef30ccfc2d535f2b8a6f2470665d17b4c197
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-06-18 15:22:12 +0200

    Add batch mode flag to Maven install command

--- builder/release.sh
+++ builder/release.sh
@@ -5,4 +5,4 @@ if ! sh ../release/2_apply_version_to_poms.sh; then exit $return_code; fi
 # all individual modules, INCLUDING the builder project must first be built
 if ! mvn clean package; then exit $return_code; fi
 # only then the EVS installation can be packaged
-if ! mvn install -f ././../distribution/pom.xml; then exit $return_code; fi
+if ! mvn install -B -f ././../distribution/pom.xml; then exit $return_code; fi
