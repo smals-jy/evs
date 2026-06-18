9868d688040d78b24d330c31862b6360e2f63cc0
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-06-18 15:22:28 +0200

    Update mvn install command to use batch mode

--- builder/release.cmd
+++ builder/release.cmd
@@ -3,4 +3,4 @@ call ../release/2_apply_version_to_poms.cmd || GOTO :EOF
 REM all individual modules, INCLUDING the builder project must first be built
 call mvn clean package || GOTO :EOF
 REM only then the EVS installation can be packaged
-call mvn install -f ././../distribution/pom.xml || GOTO :EOF
+call mvn install -B -f ././../distribution/pom.xml || GOTO :EOF
