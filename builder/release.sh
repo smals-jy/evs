15e067ac1945f835a16589541d85eb459686b6d1
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-07-08 08:26:13 +0200

    Update exit code handling in release script

--- builder/release.sh
+++ builder/release.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-
-if ! sh ../release/1_check_version_presence.sh; then exit $return_code; fi
-if ! sh ../release/2_apply_version_to_poms.sh; then exit $return_code; fi
-# all individual modules, INCLUDING the builder project must first be built
-if ! mvn clean package; then exit $return_code; fi
-# only then the EVS installation can be packaged
-if ! mvn install -B -f ././../distribution/pom.xml; then exit $return_code; fi
+#!/usr/bin/env bash  
+  
+if ! sh ../release/1_check_version_presence.sh; then exit $?; fi  
+if ! sh ../release/2_apply_version_to_poms.sh; then exit $?; fi  
+# all individual modules, INCLUDING the builder project must first be built  
+if ! mvn clean package; then exit $?; fi  
+# only then the EVS installation can be packaged  
+if ! mvn install -B -f ././../distribution/pom.xml; then exit $?; fi
