022d92450d162e652d35521812dde90693a2d78a
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-07-17 12:08:09 +0200

    Fix warnings (#1123)
    * Add EqualsAndHashCode annotation to Problem class
    
    * Add EqualsAndHashCode annotation to Treatment class
    
    * Add EqualsAndHashCode annotation to DiaryNote class

--- core/src/main/java/org/imec/ivlab/core/model/internal/parser/sumehr/Problem.java
+++ core/src/main/java/org/imec/ivlab/core/model/internal/parser/sumehr/Problem.java
@@ -3,8 +3,10 @@ package org.imec.ivlab.core.model.internal.parser.sumehr;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.imec.ivlab.core.model.internal.parser.ItemParsedItem;
 
+@EqualsAndHashCode(callSuper=false)
 @Data
 public class Problem extends ItemParsedItem {
 
