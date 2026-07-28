014c74eb520c906d80fc2a41e6cc590909ddb52a
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-07-28 13:10:52 +0200

    Update VaccinationListWriter.java

--- viewer/src/main/java/org/imec/ivlab/viewer/pdf/VaccinationListWriter.java
+++ viewer/src/main/java/org/imec/ivlab/viewer/pdf/VaccinationListWriter.java
@@ -34,7 +34,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.imec.ivlab.core.kmehr.model.util.TextTypeUtil;
 import org.imec.ivlab.core.model.internal.parser.vaccination.Vaccination;
